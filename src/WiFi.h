d674f9e8b9ab8fee3b6c189c115804190d1ca89d
Jian JIAO <Jian.Jiao5800@gmail.com>
2014-08-13 14:50:08 +0800

    使WiFi可以接受广播设备号 移除了老版本WiFi接收和发送的程序

--- src/WiFi.h
+++ src/WiFi.h
@@ -94,11 +94,8 @@ extern struct
 
 extern BYTE check_sum(const BYTE *data, WORD length);
 extern void execute_remote_cmd(const BYTE *data);
-extern int rev_remote_frame(BYTE rev);
 extern int rev_remote_frame_2(BYTE rev);
-extern void generate_remote_frame(WORD cmd, const BYTE data[], BYTE length);
 extern void generate_remote_frame_2(BYTE scr, BYTE des, WORD cmd, BYTE length, const BYTE data[]);
-extern void generate_remote_net_frame_to_send_site(WORD cmd, DWORD site, BYTE data[]);
 
 
 #endif
